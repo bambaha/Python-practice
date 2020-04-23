902948063c1c45de527c8bd348a6789fc3995a45
bambaha <41805246+bambaha@users.noreply.github.com>
2020-04-23 10:57:47 +0300

    v2

--- L02_Variables_1SPS_ex.py
+++ L02_Variables_1SPS_ex.py
@@ -1,10 +1,5 @@
-Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> amount_to_convert = 500
->>> nz_to_aus_rate = 0.95
->>> nz_dollars = amount_to_convert
->>> print ("NZ $500 = AUS $", nz_dollars * nz_to_aus_rate)
-NZ $500 = AUS $ 475.0
->>> print ("AUS $500 = NZ $", nz_dollars / nz_to_aus_rate)
-AUS $500 = NZ $ 526.3157894736843
->>> 
+amount_to_convert = 500
+nz_to_aus_rate = 0.95
+nz_dollars = amount_to_convert
+print ("NZ $500 = AUS $", nz_dollars * nz_to_aus_rate)
+print ("AUS $500 = NZ $", nz_dollars / nz_to_aus_rate)
