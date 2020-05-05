41ecfb5031ca40cd4b636a226c28275636c062df
bambaha <41805246+bambaha@users.noreply.github.com>
2020-05-05 12:21:44 +0300

    updates

--- Lec2p24.py
+++ Lec2p24.py
@@ -1,5 +1,5 @@
 amount_to_convert = 500
 nz_to_aus_rate = 0.95
 nz_dollars = amount_to_convert
-print ("NZ $500 = AUS $", nz_dollars * nz_to_aus_rate)
-print ("AUS $500 = NZ $", nz_dollars / nz_to_aus_rate)
+print('NZ $', amount_to_convert, ' = AUS $', nz_dollars * nz_to_aus_rate, sep='')
+print('AUS $', amount_to_convert, ' = NZ $', nz_dollars / nz_to_aus_rate, sep='')
--- Lec3p17.py
+++ Lec3p17.py
@@ -2,8 +2,10 @@ import math
 side1 = 4
 side2 = 7
 side3 = 9
-s = (side1 + side2 + side3) / 2
-area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
-print ("Length of sides: ", side1,', ',side2,' and ',side3, sep = "")
-Length of sides: 4, 7 and 9
-print ("Area:", area)
+
+part1 = 4*(side1**2 * side2**2 + side1**2 * side3**2 + side2**2 * side3**2)
+part2 = (side1**2 + side2**2 + side3**2)**2
+area = 1/4 * math.sqrt(part1-part2)
+
+print('Length of sides: ', side1,', ', side2, ' and ', side3, sep = '')
+print('Area:', area)
